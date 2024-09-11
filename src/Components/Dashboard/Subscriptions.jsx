89202a2d542d96e5676b8fcf087e2716c06f1418
Tushar <tusharimran470@gmail.com>
2024-09-11 20:06:49 +0600

    table added

--- src/Components/Dashboard/Subscriptions.jsx
+++ src/Components/Dashboard/Subscriptions.jsx
@@ -18,14 +18,14 @@ const Subscriptions = () => {
   });
 
   const existingUser = users.find(user => user?.email === loggedMail);
-  
+
   const [submitted, setSubmitted] = useState(false);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
 
     const transection = e.target.transection.value;
-    const transectionNo = { transection, email:loggedMail };
+    const transectionNo = { transection, email: loggedMail };
 
     const result = await axiosSecure.post('/transections', transectionNo);
 
@@ -37,14 +37,14 @@ const Subscriptions = () => {
 
   return (
     <div>
-      <h1 className="font-bold text-blue-400 text-2xl"> 
-        Hey {existingUser?.name}, Welcome to our underworld. 
+      <h1 className="font-bold text-blue-400 text-2xl">
+        Hey {existingUser?.name}, Welcome to our underworld.
       </h1>
 
       <p className="text-center font-bold my-5">User Instructions</p>
       <h3 className="mb-12">
-        Please purchase a subscription plan for 3 months with 5000 TAKA, send the money on bkash to 
-        <span className="font-bold text-lg text-red-400"> 01705458741</span>, 
+        Please purchase a subscription plan for 3 months with 5000 TAKA, send the money on bkash to
+        <span className="font-bold text-lg text-red-400"> 01705458741</span>,
         and submit the transaction number in the box below. Admin will manually check your transaction and approve you within the next 12 hours.
       </h3>
 
@@ -59,11 +59,11 @@ const Subscriptions = () => {
           />
         )}
 
-        <input 
-          className="bg-blue-400 text-white font-semibold py-2 px-4 rounded-lg hover:bg-blue-500 transition-all ease-in-out duration-200" 
-          type="submit" 
-          value={submitted? "Already Submitted please wait for admin response": 'Submit'} 
-          disabled={submitted} 
+        <input
+          className="bg-blue-400 text-white font-semibold py-2 px-4 rounded-lg hover:bg-blue-500 transition-all ease-in-out duration-200"
+          type="submit"
+          value={submitted ? "Already Submitted please wait for admin response" : 'Submit'}
+          disabled={submitted}
         />
       </form>
     </div>
--- src/Components/Dashboard/TransectionNo.jsx
+++ src/Components/Dashboard/TransectionNo.jsx
@@ -1,8 +1,56 @@
+import { useQuery } from "@tanstack/react-query";
+import useAxiosSecure from "../../Hooks/useAxiosSecure";
 
 const TransectionNo = () => {
+    const { data: transections = [] } = useQuery({
+        queryKey: ['transections'],
+        queryFn: async () => {
+          const res = await useAxiosSecure.get(`/transections`);
+          return res.data;
+        }
+      });
+
+      console.log('g', transections)
+
     return (
         <div>
-            Transections
+             <div className="overflow-x-auto">
+  <table className="table table-zebra">
+    {/* head */}
+    <thead>
+      <tr>
+        <th></th>
+        <th>Name</th>
+        <th>Job</th>
+        <th>Favorite Color</th>
+      </tr>
+    </thead>
+    <tbody>
+      {/* row 1 */}
+      <tr>
+        <th>1</th>
+        <td>Cy Ganderton</td>
+        <td>Quality Control Specialist</td>
+        <td>Blue</td>
+      </tr>
+      {/* row 2 */}
+      <tr>
+        <th>2</th>
+        <td>Hart Hagerty</td>
+        <td>Desktop Support Technician</td>
+        <td>Purple</td>
+      </tr>
+      {/* row 3 */}
+      <tr>
+        <th>3</th>
+        <td>Brice Swyre</td>
+        <td>Tax Accountant</td>
+        <td>Red</td>
+      </tr>
+    </tbody>
+  </table>
+</div>
+             
         </div>
     );
 };
